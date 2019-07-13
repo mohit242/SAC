d774c0930538fbb376bc2fb577071305d1a2744c
mohit242 <f2014074@pilani.bits-pilani.ac.in>
2019-07-14 05:06:19 +0530

    chore: Added setup script.

--- setup.sh
+++ setup.sh
@@ -7,3 +7,5 @@ apt-get update && apt-get install cmake libopenmpi-dev python3-dev zlib1g-dev
 pip install stable-baselines
 pip install gym
 
+git config --global user.name "$NAME_GIT"
+git config --global user.email "$EMAIL_GIT"
