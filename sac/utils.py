ba5a08801c829478895b10450b7bfef6a2c581af
mohit242 <f2014074@pilani.bits-pilani.ac.in>
2019-07-16 05:38:09 +0530

    feat: Action Wrapper for SAC

--- sac/utils.py
+++ sac/utils.py
@@ -1,5 +1,6 @@
 import torch
 import numpy as np
+import gym
 
 
 def soft_update(target, source, polyak=0.001):
@@ -12,4 +13,20 @@ def hard_update(target, source):
         target_param.data.copy_(local_param.data)
 
 
-#TODO Wrapper to squash actions in [-1, 1]
+class ActionSACWrapper(gym.ActionWrapper):
+
+    def __init__(self, env):
+        """Changes action space range from [action_space.low, action_space.high] to [-1,1]"""
+        self.env = env
+        super().__init__(env)
+        self.action_space = env.action_space
+        self.action_space.low = -np.ones_like(env.action_space.low)
+        self.action_space.high = np.ones_like(env.action_space.high)
+
+    def action(self, action):
+        action = (action * (self.env.action_space.high - self.env.action_space.low)/2) +\
+                 (self.env.action_space.high + self.env.action_space.low)/2
+        return action
+
+
+
